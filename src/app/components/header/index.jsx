a15b01628f755bc25d15657c4706c24de458228b
SoloLineDefender <kestpro35@gmail.com>
2021-06-07 09:15:17 +0300

    change style and logo

--- src/app/components/header/index.jsx
+++ src/app/components/header/index.jsx
@@ -5,15 +5,16 @@ const HeaderComponent = () => {
         <header className="header" id="header">
             <div className="container">
                 <div className="logo-container">
-                    <div className="logo-img"/>
-                    <h1 className="breand"></h1>
+                    <a href="/" className="logo-img"></a>
+                    {/* <div className="logo-img"/> */}
+                    <h1 className="breand">JUST TALK</h1>
                 </div>
                 <div className="search-container">
                     <img src='/logo/logo-breand.png' alt="" className="search-img" />
                     <input type="text" className="search-box" />
                 </div>
                 <div className="profile-container">
-                    <a href="" className="profile"></a>
+                    <a href="/profile" className="profile-img"></a>
                 </div>
             </div>
         </header>
