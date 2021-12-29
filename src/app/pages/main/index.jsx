083e2ebff588ca58bb035a10dc4edab6d2df65e4
SoloLineDefender <72494398+SoloLineDefender@users.noreply.github.com>
2021-12-29 14:36:41 +0300

    start-projekt

--- src/app/pages/main/index.jsx
+++ src/app/pages/main/index.jsx
@@ -4,11 +4,16 @@ const MainPage = () => {
 
     useEffect(() => {
         document.title = 'Главная'
+        
     })
+
+    function test (){
+        console.log("document.width")
+    };
 
     return (
         <React.Fragment>
-            
+            <div className="mainWindow">{test()}</div>
         </React.Fragment>
     )
 }
