046047178b77eddf7077df47687d4d6e80810a5e
SoloLineDefender <72494398+SoloLineDefender@users.noreply.github.com>
2022-01-10 16:40:13 +0300

    see_how_work_window_and_how_work_read_text_from_input

--- src/app/pages/main/index.jsx
+++ src/app/pages/main/index.jsx
@@ -6,16 +6,50 @@ const MainPage = () => {
         document.title = 'Главная'
         
     })
+    
+    var main = document.getElementById("renderer")
+    var hi = window.innerHeight
+    var wi = window.innerWidth
+    var a = {hi,wi}
+
+    
 
     function test (){
-        console.log("document.width")
+        return (
+            console.log(a.wi,a.hi)
+        )
+        
     };
 
+    function test2(){
+        {setInterval(() => {
+            test()
+        }, 3000)}
+    }
+
+
+    
+    function test3(){
+        setInterval(() => {
+            return(console.log(inputText.value))            
+        }, 3000);
+
+    }
+    
     return (
         <React.Fragment>
-            <div className="mainWindow">{test()}</div>
+            
+            <div className="mainWindow" id="MainWindow" name="mainWindow">
+                <input type="text" id="WriteText" className="text" name="text" value="1234" />
+                ssss
+            </div>
+            
+            {test2()}
+            {test3()}
         </React.Fragment>
     )
+
+
 }
 
 export default MainPage
