e9453652df62f864754d1e7e6a4bd2240fc6cf21
SoloLineDefender <72494398+SoloLineDefender@users.noreply.github.com>
2022-01-11 16:54:12 +0300

    Revork_input_text_from_boxinput

--- src/app/pages/main/index.jsx
+++ src/app/pages/main/index.jsx
@@ -15,8 +15,10 @@ const MainPage = () => {
     
 
     function test (){
+        let a = document.getElementById("WriteText")
+        let b = document.getElementById("")
         return (
-            console.log(a.wi,a.hi)
+            console.log(a.value)
         )
         
     };
@@ -30,22 +32,28 @@ const MainPage = () => {
 
     
     function test3(){
+        var a = document.getElementById("textSomeOne")
         setInterval(() => {
-            return(console.log(inputText.value))            
+            return(console.log(a))            
         }, 3000);
-
+        // if (onchange = true) {
+        //     let textInput = document.getElementById("WriteText")
+        //     let porag = document.getElementById("textSome")
+        //     porag.innerText = textInput.value
+        //     }
+            
     }
     
     return (
         <React.Fragment>
             
             <div className="mainWindow" id="MainWindow" name="mainWindow">
-                <input type="text" id="WriteText" className="text" name="text" value="1234" />
-                ssss
+                <input type="text" id="WriteText" className="text" name="text" onChange={test}/>
+                <p className="textSome" id="textSomeOne">111</p>
             </div>
             
-            {test2()}
-            {test3()}
+            {/* {test2()}
+            {test3()} */}
         </React.Fragment>
     )
 
